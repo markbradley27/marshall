7a3a53394867a3a5d64a072e6fdcb564a3c7d3c1
Mark Bradley <markbradley27@gmail.com>
2021-07-28 09:44:08 -0400

    Add load_mountains.py script for populating db from DBpedia.

---
+++ src/tools/load_mountains.py
@@ -0,0 +1,70 @@
+#! /usr/bin/env python3
+
+# TODO: Pytype annotations.
+
+from absl import app
+from absl import flags
+from absl import logging
+import json
+import pymongo
+import SPARQLWrapper
+
+FLAGS = flags.FLAGS
+
+flags.DEFINE_integer('n', 1, 'Number of mountains to retrieve (anything less than 0 retrieves all).')
+flags.DEFINE_integer('sparql_page_size', 100, 'Max number of mountain URIs to retrieve at once.')
+flags.DEFINE_string('mongodb_endpoint', '127.0.0.1', 'Endpoint of the mongodb server to populate.')
+flags.DEFINE_string('mongodb_db', 'marshall', 'DB name to populate.')
+flags.DEFINE_string('mongodb_collection', 'mountains', 'DB collection to populate.')
+
+_DBPEDIA_SPARQL_ENDPOINT = "http://dbpedia.org/sparql"
+
+def parseSparqlMountain(sparql_json):
+    parsed = {}
+    for spo in sparql_json["results"]["bindings"]:
+        parsed[spo["p"]["value"]] = spo["o"]["value"]
+    return parsed
+
+def getMountain(uri):
+    sparql = SPARQLWrapper.SPARQLWrapper(_DBPEDIA_SPARQL_ENDPOINT)
+    sparql.setQuery("describe <{}>".format(uri))
+    sparql.setReturnFormat(SPARQLWrapper.JSON)
+    result = sparql.query().convert()
+    return parseSparqlMountain(result)
+
+def getMountains(n):
+    offset = 0
+    base_query = "select * {?mountain a dbo:Mountain}"
+    while offset < n or n < 0:
+        limit = FLAGS.sparql_page_size
+        if n >= 0:
+            limit = min(limit, n - offset)
+        query = "{} LIMIT {} OFFSET {}".format(base_query, limit, offset)
+        logging.info("Querying: %s", query)
+
+        sparql = SPARQLWrapper.SPARQLWrapper(_DBPEDIA_SPARQL_ENDPOINT)
+        sparql.setQuery(query)
+        sparql.setReturnFormat(SPARQLWrapper.JSON)
+        results = sparql.query().convert()
+
+        bindings = results["results"]["bindings"]
+        if len(bindings) == 0:
+            return
+        for result in bindings:
+            uri = result["mountain"]["value"]
+            yield uri, getMountain(uri)
+
+        offset += FLAGS.sparql_page_size
+
+def main(argv):
+    mongo_client = pymongo.MongoClient(FLAGS.mongodb_endpoint)
+    db = mongo_client[FLAGS.mongodb_db]
+    collection = db[FLAGS.mongodb_collection]
+
+    for uri, mountain in getMountains(FLAGS.n):
+        logging.info("Inserting: {}".format(uri))
+        mountain["_id"] = uri
+        collection.insert_one(mountain)
+
+if __name__ == "__main__":
+   app.run(main)
