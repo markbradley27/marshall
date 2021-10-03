080ebe954f8fc6784c70ded0cdaf400aabfbbd8e
Mark Bradley <markbradley27@gmail.com>
2021-10-02 23:14:51 -0400

    Remove some tools that aren't relevant any more.

--- tools/init_db.sql
+++
@@ -1,4 +0,0 @@
-CREATE EXTENSION postgis;
-
-CREATE TABLE mountains(uri TEXT PRIMARY KEY, loc geography(POINTZ), raw_data JSONB);
-CREATE TABLE activities(id BIGINT PRIMARY KEY, path geography(LINESTRING), raw_data JSONB);
