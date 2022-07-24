5224a22dfe18ed59552d3b21c425aff9a636e2ec
Mark Bradley <markbradley27@gmail.com>
2022-07-24 15:31:34 -0700

    Disable mountain typeahead while mountains are loading.

--- app/src/components/pages/add_ascent/AddAscentForm.tsx
+++ app/src/components/pages/add_ascent/AddAscentForm.tsx
@@ -16,7 +16,7 @@ interface AddAscentFormProps {
 export default function AddAscentForm(props: AddAscentFormProps) {
   const auth = useAuth();
 
-  const [mountains, setMountains] = useState<MountainState[] | null>(null);
+  const [mountains, setMountains] = useState<MountainState[]>([]);
   const [alertMessage, setAlertMessage] = useState("");
 
   const [typeaheadFocused, setTypeaheadFocused] = useState(false);
@@ -101,132 +101,135 @@ export default function AddAscentForm(props: AddAscentFormProps) {
           {alertMessage}
         </Alert>
       )}
-      {mountains != null && (
-        <Formik
-          initialValues={{
-            mountain: undefined,
-            date: "",
-            time: "",
-            privacy: auth.users?.db?.defaultAscentPrivacy,
-          }}
-          onSubmit={submitAscent}
-          validate={validate}
-        >
-          {({
-            errors,
-            getFieldProps,
-            handleSubmit,
-            isSubmitting,
-            setFieldTouched,
-            setFieldValue,
-            touched,
-          }) => (
-            <Form
-              noValidate
-              onSubmit={(...args) => {
-                handleSubmit(...args);
-                // Bit of a hack; without this, touched.mountain will be
-                // undefined if the mountain typeahead has been touched but then
-                // cleared. Don't know why, but this works around it.
-                setFieldTouched("mountain");
-              }}
-            >
-              <Stack gap={3}>
-                <Form.Group controlId="mountain">
-                  <Form.Label>Mountain</Form.Label>
-                  <span ref={mountainTypeaheadWrapper}>
-                    <Typeahead
-                      id="mountain"
-                      isInvalid={
-                        !typeaheadFocused &&
-                        touched.mountain &&
-                        errors.mountain != null
-                      }
-                      labelKey="name"
-                      onBlur={() => {
-                        setTypeaheadFocused(false);
-                        // handleBlur doesn't seem to handle mountain being an
-                        // object well, so I just set the field touched directly.
-                        setFieldTouched("mountain");
-                      }}
-                      onChange={(selected) => {
-                        setFieldValue(
-                          "mountain",
-                          selected[0] != null ? selected[0] : undefined
-                        );
-                      }}
-                      onFocus={() => {
-                        setTypeaheadFocused(true);
-                      }}
-                      options={mountains}
-                      placeholder="Search by mountain name..."
-                      ref={mountainTypeahead}
-                    />
-                  </span>
+      <Formik
+        initialValues={{
+          mountain: undefined,
+          date: "",
+          time: "",
+          privacy: auth.users?.db?.defaultAscentPrivacy,
+        }}
+        onSubmit={submitAscent}
+        validate={validate}
+      >
+        {({
+          errors,
+          getFieldProps,
+          handleSubmit,
+          isSubmitting,
+          setFieldTouched,
+          setFieldValue,
+          touched,
+        }) => (
+          <Form
+            noValidate
+            onSubmit={(...args) => {
+              handleSubmit(...args);
+              // Bit of a hack; without this, touched.mountain will be
+              // undefined if the mountain typeahead has been touched but then
+              // cleared. Don't know why, but this works around it.
+              setFieldTouched("mountain");
+            }}
+          >
+            <Stack gap={3}>
+              <Form.Group controlId="mountain">
+                <Form.Label>Mountain</Form.Label>
+                <span ref={mountainTypeaheadWrapper}>
+                  <Typeahead
+                    disabled={mountains.length === 0}
+                    id="mountain"
+                    isInvalid={
+                      !typeaheadFocused &&
+                      touched.mountain &&
+                      errors.mountain != null
+                    }
+                    labelKey="name"
+                    onBlur={() => {
+                      setTypeaheadFocused(false);
+                      // handleBlur doesn't seem to handle mountain being an
+                      // object well, so I just set the field touched directly.
+                      setFieldTouched("mountain");
+                    }}
+                    onChange={(selected) => {
+                      setFieldValue(
+                        "mountain",
+                        selected[0] != null ? selected[0] : undefined
+                      );
+                    }}
+                    onFocus={() => {
+                      setTypeaheadFocused(true);
+                    }}
+                    options={mountains}
+                    placeholder={
+                      mountains.length === 0
+                        ? "Loading..."
+                        : "Search by mountain name..."
+                    }
+                    ref={mountainTypeahead}
+                  />
+                </span>
+                {!typeaheadFocused && (
+                  <InvalidTooltip
+                    error={errors.mountain}
+                    target={mountainTypeaheadWrapper.current}
+                    touched={touched.mountain}
+                  />
+                )}
+              </Form.Group>
+              <Stack direction="horizontal" gap={3}>
+                <Form.Group className="position-relative" controlId="date">
+                  <Form.Label>Date</Form.Label>
+                  <Form.Control
+                    isInvalid={touched.date && errors.date != null}
+                    ref={dateControl}
+                    type="date"
+                    {...getFieldProps("date")}
+                  />
                   {!typeaheadFocused && (
                     <InvalidTooltip
-                      error={errors.mountain}
-                      target={mountainTypeaheadWrapper.current}
-                      touched={touched.mountain}
+                      error={errors.date}
+                      target={dateControl.current}
+                      touched={touched.date}
                     />
                   )}
                 </Form.Group>
-                <Stack direction="horizontal" gap={3}>
-                  <Form.Group className="position-relative" controlId="date">
-                    <Form.Label>Date</Form.Label>
-                    <Form.Control
-                      isInvalid={touched.date && errors.date != null}
-                      ref={dateControl}
-                      type="date"
-                      {...getFieldProps("date")}
+                <Form.Group>
+                  <Form.Label>Time</Form.Label>
+                  <Form.Control
+                    isInvalid={touched.time && errors.time != null}
+                    ref={timeControl}
+                    type="time"
+                    {...getFieldProps("time")}
+                  />
+                  {!typeaheadFocused && (
+                    <InvalidTooltip
+                      error={errors.time}
+                      target={timeControl.current}
+                      touched={touched.time}
                     />
-                    {!typeaheadFocused && (
-                      <InvalidTooltip
-                        error={errors.date}
-                        target={dateControl.current}
-                        touched={touched.date}
-                      />
-                    )}
-                  </Form.Group>
-                  <Form.Group>
-                    <Form.Label>Time</Form.Label>
-                    <Form.Control
-                      isInvalid={touched.time && errors.time != null}
-                      ref={timeControl}
-                      type="time"
-                      {...getFieldProps("time")}
-                    />
-                    {!typeaheadFocused && (
-                      <InvalidTooltip
-                        error={errors.time}
-                        target={timeControl.current}
-                        touched={touched.time}
-                      />
-                    )}
-                  </Form.Group>
-                  <Form.Group>
-                    <Form.Label>Visibility</Form.Label>
-                    <Form.Select
-                      defaultValue={auth.users?.db?.defaultAscentPrivacy}
-                      ref={privacySelect}
-                    >
-                      <option value="PUBLIC">Public</option>
-                      <option value="FOLLOWERS_ONLY">Followers Only</option>
-                      <option value="PRIVATE">Private</option>
-                    </Form.Select>
-                  </Form.Group>
-                </Stack>
-                <Button
-                  className={"w-100" + (isSubmitting ? " disabled" : "")}
-                  type="submit"
-                >
-                  {isSubmitting ? "Submitting..." : "Submit"}
-                </Button>
+                  )}
+                </Form.Group>
+                <Form.Group>
+                  <Form.Label>Visibility</Form.Label>
+                  <Form.Select
+                    defaultValue={auth.users?.db?.defaultAscentPrivacy}
+                    ref={privacySelect}
+                  >
+                    <option value="PUBLIC">Public</option>
+                    <option value="FOLLOWERS_ONLY">Followers Only</option>
+                    <option value="PRIVATE">Private</option>
+                  </Form.Select>
+                </Form.Group>
               </Stack>
-            </Form>
-          )}
-        </Formik>
-      )}
+              <Button
+                className={"w-100" + (isSubmitting ? " disabled" : "")}
+                type="submit"
+              >
+                {isSubmitting ? "Submitting..." : "Submit"}
+              </Button>
+            </Stack>
+          </Form>
+        )}
+      </Formik>
     </>
   );
 }
