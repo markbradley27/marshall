3883dad412e8fe45465df3612bbc3ce0c428419e
Mark Bradley <markbradley27@gmail.com>
2022-09-30 11:27:06 -0700

    GoogleMap can display a path.

--- app/src/components/shared/map/GoogleMap.tsx
+++ app/src/components/shared/map/GoogleMap.tsx
@@ -1,10 +1,11 @@
 import {
   GoogleMap as ReactGoogleMap,
   MarkerClusterer,
+  Polyline,
 } from "@react-google-maps/api";
 import { MountainState } from "api/mountain_endpoints";
 import { geoJsonToCoords } from "api/util";
-import { Feature, FeatureCollection } from "geojson";
+import { Feature, FeatureCollection, LineString, Position } from "geojson";
 import toBBox from "geojson-bounding-box";
 import useGoogleMaps from "hooks/loadGoogleMaps";
 import { useCallback } from "react";
@@ -24,20 +25,24 @@ function getLatLngBounds(props: GoogleMapProps) {
     features: [],
   } as FeatureCollection;
 
+  if (props.path != null) {
+    featureCollection.features.push({
+      type: "Feature",
+      geometry: props.path,
+    } as Feature);
+  }
   if (props.primary != null) {
-    const primaryFeature = {
+    featureCollection.features.push({
       type: "Feature",
       geometry: props.primary.location,
-    } as Feature;
-    featureCollection.features.push(primaryFeature);
+    } as Feature);
   }
   if (props.secondaries != null) {
     for (const secondary of props.secondaries) {
-      const secondaryFeature = {
+      featureCollection.features.push({
         type: "Feature",
         geometry: secondary.location,
-      } as Feature;
-      featureCollection.features.push(secondaryFeature);
+      } as Feature);
     }
   }
   const bbox = toBBox(featureCollection);
@@ -54,7 +59,10 @@ function getLatLngBounds(props: GoogleMapProps) {
   );
 }
 
+// TODO: Add option to disable clustering.
 interface GoogleMapProps {
+  // Path that should be displayed on the map.
+  path?: LineString;
   // Main mountain that should be highlighted on the map.
   primary?: MountainState;
   // Array of mountains that should also be displayed on the map but less
@@ -67,21 +75,25 @@ export default function GoogleMap(props: GoogleMapProps) {
 
   const onLoad = useCallback(
     (map: google.maps.Map) => {
-      // TODO: Finish this.
-      if (props.secondaries) {
-        map.fitBounds(getLatLngBounds(props));
-      } else if (props.primary != null) {
+      if (
+        props.primary != null &&
+        props.secondaries == null &&
+        props.path == null
+      ) {
         map.setCenter({
           lng: props.primary.location.coordinates[0],
           lat: props.primary.location.coordinates[1],
         });
         map.setZoom(12);
+      } else {
+        map.fitBounds(getLatLngBounds(props));
       }
     },
     [props]
   );
 
-  return googleMapsLoaded && (props.primary || props.secondaries) ? (
+  return googleMapsLoaded &&
+    (props.primary || props.secondaries || props.path) ? (
     <ReactGoogleMap
       mapContainerStyle={MAP_CONTAINER_STYLE}
       options={{
@@ -90,6 +102,15 @@ export default function GoogleMap(props: GoogleMapProps) {
       }}
       onLoad={onLoad}
     >
+      {props.path && (
+        <Polyline
+          options={{ strokeColor: "#fc5200" }}
+          path={props.path.coordinates.map((point: Position) => ({
+            lng: point[0],
+            lat: point[1],
+          }))}
+        />
+      )}
       {props.primary && (
         <MountainMarker coords={geoJsonToCoords(props.primary.location)} />
       )}
